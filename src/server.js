40cd110aa9865cb2acc2b2b4b3976e678a87cd60
Dan Niemi <niemdani@gmail.com>
2024-11-08 19:07:23 -0600

    fixed

--- src/server.js
+++ src/server.js
@@ -1,28 +1,38 @@
 let rooms = [] //room IDs
-const BOARDSIZE = 800
+const WORLDWIDTH = 8000
+const WORLDHEIGHT = 8000
+const SPAWNRADIUS = 800
 const RADMIN = 20
 const RADMAX = 120
 const POINTSMIN = 5
 const POINTSMAX = 12
 const NUMROCKS = 10
 
+
 class PartyServer {
   constructor(room) {
     this.rockCounter = 0;
+    this.spawnAttempts = 0;
     this.room = room;
-    rooms.push(this.room.id)
     this.cursors = [];
     this.rocks = [];
-    for (let i = 0; i < NUMROCKS; i++) {
-      let p = { x: Math.random() * BOARDSIZE, y: Math.random() * BOARDSIZE }
+    rooms.push(this.room.id)
+    while (this.rocks.length < NUMROCKS && this.spawnAttempts < 100) {
+      let pos = { x: Math.random() * SPAWNRADIUS , y: Math.random() * SPAWNRADIUS}
+      // let pos = { x: Math.random() * SPAWNRADIUS + WORLDWIDTH/2 - SPAWNRADIUS/2, y: Math.random() * SPAWNRADIUS + WORLDHEIGHT/2 - SPAWNRADIUS/2 }
+      let rad = Math.random() * (RADMAX - RADMIN) + RADMIN;
+      if (this.rocks.some(rock => dist(rock.pos, pos) < rad + rock.rad)) {
+        console.log(this.spawnAttempts++)
+        continue //if too close to any, go to next loop to get a new random position
+      }
       let id = 'rock' + this.rockCounter++
-      this.rocks.push(this.newRock(id, p))
+      this.rocks.push(this.newRock(id, pos, rad))
     }
-    
+
   }
   onConnect(conn, _ctx) {
     this.cursors.push({ id: conn.id, pos: { x: 0, y: 0 } });
-    conn.send(JSON.stringify({ type: "connectionSelf", id: conn.id,rocks: this.rocks,room:this.room.id }));
+    conn.send(JSON.stringify({ type: "connectionSelf", id: conn.id, rocks: this.rocks, room: this.room.id,worldWidth:WORLDWIDTH,worldHeight:WORLDHEIGHT }));
     this.room.broadcast(JSON.stringify({ type: "connection", rooms: rooms, cursors: this.cursors }));
   }
 
@@ -54,18 +64,16 @@ class PartyServer {
   onClose(conn) {
     this.cursors = this.cursors.filter(cursor => cursor.id !== conn.id)
     this.room.broadcast(JSON.stringify({ type: "cursorRemove", cursors: this.cursors, }));
-    if([...this.room.getConnections()].length<1){
+    if ([...this.room.getConnections()].length < 1) {
       rooms = rooms.filter(room => room !== this.room)
     }
 
   }
 
 
-  newRock(id, pos) {
-    this.id = id
+  newRock(id, pos, rad) {
     let points = []
     let numPoints = Math.random() * (POINTSMAX - POINTSMIN) + POINTSMIN;
-    let rad = Math.random() * (RADMAX - RADMIN) + RADMIN;
     // make points    
     for (let j = 0; j < numPoints; j++) {
       let angle = Math.random() * Math.PI * 2
@@ -80,9 +88,12 @@ class PartyServer {
     points.sort((a, b) => Math.atan2(b.y - cy, b.x - cx) - Math.atan2(a.y - cy, a.x - cx))
     return { pos: pos, points: points, rad: rad, radMax: RADMAX, id: id }
   }
-  
+
 }
 
 export default PartyServer;
 
 
+function dist(vec1, vec2) {
+  return Math.hypot(vec2.x - vec1.x, vec2.y - vec1.y)
+}
